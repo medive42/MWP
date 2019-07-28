3022712ab3ee86e0f18263996897a1b65c88e762
Shawn <medivh@live.com>
2019-07-28 09:25:51 +0800

    12
    12222

--- wp-content/themes/gmyk/js/stat.js
+++
@@ -1,6 +0,0 @@
-//var currenturl = document.location.href;
-//var preurl=document.referrer;
-//var title = document.title ;
-var typeid = $('#typeid').val();
-var aid = $('#aid').val();
-$.post('clickcount.php.htm' , {typeid:typeid,aid:aid},function(ret){});
